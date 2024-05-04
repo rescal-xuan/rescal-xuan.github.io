a2a2c4d49fd5b961debda99783e0446afbd08d94
rescal-xuan <3528657344@qq.com>
2024-05-04 23:27:17 +0800

    Site updated: 2024-05-04 23:27:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/04/hello-world/"];function toRandomPost(){
+var posts=["2024/05/04/hello-world/","2024/05/04/基于github和Hexo 搭建自己的博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
