3fd548575789bb7407c0a7db57388f92218fc975
rescal-xuan <3528657344@qq.com>
2024-05-05 17:25:59 +0800

    Site updated: 2024-05-05 17:25:59

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/04/hello-world/","2024/05/04/搭建自己的博客/"];function toRandomPost(){
+var posts=["2024/05/04/hello-world/","2024/05/04/搭建自己的博客/","2024/05/05/机器学习（-数据科学）的工作流程/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
